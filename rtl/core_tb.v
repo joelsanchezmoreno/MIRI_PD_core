bc81d266ec04998e39c747841b346ec84daced21
joel.sanchez <joel.sanchez@esperantotech.com>
2020-12-05 17:34:15 +0100

    [Core TB] First stage: added FIFO for both I$ and D$ requests, need to finish integration

--- rtl/core_tb.v
+++ rtl/core_tb.v
@@ -17,12 +17,10 @@ end
 // Request from D$ to the memory hierarchy
 logic                                   dcache_req_valid_miss;
 memory_request_t                        dcache_req_info_miss;
-logic [`THR_PER_CORE_WIDTH-1:0]         dcache_req_thread_id;
 
 // Request from I$ to the memory hierarchy
 logic                                   icache_req_valid_miss;
 memory_request_t                        icache_req_info_miss;
-logic [`THR_PER_CORE_WIDTH-1:0]         icache_req_thread_id;
 
 // Response from the memory hierarchy
 logic [`DCACHE_LINE_WIDTH-1:0]          rsp_data_miss;
@@ -46,12 +44,10 @@ core_top
     // Request from I$ to the memory hierarchy
     .dcache_req_valid_miss  ( dcache_req_valid_miss ),
     .dcache_req_info_miss   ( dcache_req_info_miss  ),
-    .dcache_req_thread_id   ( dcache_req_thread_id  ),
 
     // Request from D$ to the memory hierarchy                                      
     .icache_req_valid_miss  ( icache_req_valid_miss ),
     .icache_req_info_miss   ( icache_req_info_miss  ),
-    .icache_req_thread_id   ( icache_req_thread_id  ),
                                       
     // Response from the memory hierarchy                                  
     .rsp_data_miss          ( rsp_data_miss         ),
@@ -68,125 +64,132 @@ logic [`MAIN_MEMORY_LINE_RANGE] main_memory [`MAIN_MEMORY_DEPTH_RANGE];
 
 // Request from core arbiter to MM
 logic               req_mm_valid;
+logic               req_mm_valid_ff;
 memory_request_t    req_mm_info;
 memory_request_t    req_mm_info_ff;
+logic req_mm_info_is_dcache;
+logic req_mm_info_is_dcache_ff;
 
-//  CLK    DOUT            DIN           
-`FF(clk_i, req_mm_info_ff, req_mm_info)
+    //     CLK    RST    DOUT             DIN           DEF
+`RST_EN_FF(clk_i, rst_i, req_mm_valid_ff, req_mm_valid, 1'b0)
+
+//  CLK    DOUT                      DIN           
+`FF(clk_i, req_mm_info_ff,           req_mm_info)
+`FF(clk_i, req_mm_info_is_dcache_ff, req_mm_info_is_dcache)
 
 // Response from MM to core arbiter
 logic rsp_mm_valid;
+logic rsp_mm_ready;
 logic rsp_mm_bus_error;
 logic [`ICACHE_LINE_WIDTH-1:0]  rsp_mm_data;
 
 //////////////////////////////////////////////////
-// Arbiter logic
-//
-// Arbiter between instruction cache and data cache. 
-// D$ has always priority except if we are performing an instruction cache
-// request
+// FIFO - Arbiter logic
+// 
+// Each thread can have two requests in flight (Fetch and Cache)
+// Two requests can be received at the same cycle (Fetch and Cache)
 
-// Request from D$ to the memory hierarchy
-logic                           dcache_req_valid_next;
-logc                            dcache_req_valid_ff;
-logic [`THR_PER_CORE_WIDTH-1:0] dcache_thread_id_ff; // thread id required???
-memory_request_t                dcache_req_info_ff;  //TODO: Maybe storage has to bee an array for threads
+logic               push_dcache_fifo;
+logic               pop_dcache_fifo;
+logic               dcache_fifo_not_empty;
+memory_request_t    dcache_pop_info;
 
+fifo
+#(
+  .WIDTH ( $bits(dcache_req_info_miss)),
+  .DEPTH ( `THR_PER_CORE              ) 
+)
+main_memory_dcache_fifo
+(
+    // System signals
+    .clock      ( clock                     ),
+    .reset      ( reset                     ),
+    .full       (                           ) 
 
-// Request from I$ to the memory hierarchy
-logic                           icache_req_valid_next;
-logic                           icache_req_valid_ff;
-logic [`THR_PER_CORE_WIDTH-1:0] icache_thread_id_ff; // thread id required???
-memory_request_t                icache_req_info_ff; //TODO: Maybe storage has to bee an array for threads
+    // Push data
+    .push       ( push_dcache_fifo          ),
+    .wdata      ( dcache_req_info_miss      ),                                                   
 
-//      CLK    RST      DOUT                 DIN           DEF
-`RST_FF(clk_i, reset_i, dcache_req_valid_ff, dcache_req_valid_next, '0)
-`RST_FF(clk_i, reset_i, icache_req_valid_ff, icache_req_valid_next, '0)
+    // Pop data
+    .pop        ( pop_dcache_fifo           ),
+    .rdata      ( dcache_pop_info           ),
+    .valid      ( dcache_fifo_not_empty     ),
+);
 
-//         CLK    RST      EN                     DOUT                 DIN                   DEF
-`RST_EN_FF(clk_i, reset_i, dcache_req_valid_miss, dcache_req_info_ff,  dcache_req_info_miss, '0)
-`RST_EN_FF(clk_i, reset_i, dcache_req_valid_miss, dcache_thread_id_ff, dcache_thread_id,     '0)
-`RST_EN_FF(clk_i, reset_i, icache_req_valid_miss, icache_req_info_ff,  icache_req_info_miss, '0)
-`RST_EN_FF(clk_i, reset_i, icache_req_valid_miss, icache_thread_id_ff, icache_thread_id,     '0)
+logic               push_icache_fifo;
+logic               pop_icache_fifo;
+logic               icache_fifo_not_empty;
+memory_request_t    icache_pop_info;
 
-logic   wait_rsp_icache_next;
-lgoci   wait_rsp_icache_ff ;
-logic   wait_rsp_enable;
-logic   wait_icache_rsp_update;
+fifo
+#(
+  .WIDTH ( $bits(icache_req_info_miss)),
+  .DEPTH ( `THR_PER_CORE              ) 
+)
+main_memory_icache_fifo
+(
+    // System signals
+    .clock      ( clock                     ),
+    .reset      ( reset                     ),
+    .full       (                           ) 
 
-assign wait_rsp_enable = (!dcache_req_valid_miss & icache_req_valid_miss) | wait_icache_rsp_update;
+    // Push data
+    .push       ( push_icache_fifo          ),
+    .wdata      ( icache_req_info_miss      ),                                                   
 
-//         CLK    RST      EN               DOUT                DIN                   DEF
-`RST_EN_FF(clk_i, reset_i, wait_rsp_enable, wait_rsp_icache_ff, wait_rsp_icache_next, '0)
+    // Pop data
+    .pop        ( pop_icache_fifo           ),
+    .rdata      ( icache_pop_info           ),
+    .valid      ( icache_fifo_not_empty     ),
+);
+
 
 always_comb
 begin
+        // Response to core
     rsp_valid_miss  = 1'b0;
     rsp_bus_error   = rsp_mm_bus_error;
 
-        // Hold values for next cycles
-    dcache_req_valid_next = dcache_req_valid_ff;
-    icache_req_valid_next = icache_req_valid_ff;
-    req_mm_info           = req_mm_info_ff;
+        // Push & Pop FIFOs
+    push_dcache_fifo   = dcache_req_valid_miss;
+    push_icache_fifo   = icache_req_valid_miss;
+    pop_dcache_fifo    = 1'b0;
+    pop_icache_fifo    = 1'b0;
 
-    // We store that we have a pending request from D$
-    if (dcache_req_valid_miss)
+        // Request to Main Memory
+    req_mm_valid = req_mm_valid_ff;
+    req_mm_info  = req_mm_info_ff;
+    req_mm_info_is_dcache   = req_mm_info_is_dcache_ff;
+
+    // If there is a pendent request from D$ or I$ and Main Memory is ready.
+    // Then, send the request to Main Memory
+    if ( (dcache_fifo_not_empty | icache_fifo_not_empty) 
+        & rsp_mm_ready )
     begin
-        dcache_req_valid_next   = 1'b1;
+        req_mm_valid = 1'b1;
+
+        //TODO: Think how to ensure the order of pop is correct between both FIFOs
+        //TODO: Add logic to choose between sending D$ or I$
+        pop_dcache_fifo = XXX;
+        pop_icache_fifo = XXX;
+
+        req_mm_info_is_dcache = pop_dcache_fifo;
+        req_mm_info  = (pop_dcache_fifo) ? dcache_pop_info : 
+                                           icache_pop_info;
     end
 
-    // We store that we have a pending request from I$    
-    if (icache_req_valid_miss)
+    if(rsp_mm_valid)
     begin
-        icache_req_valid_next   = 1'b1;
-    end
-
-    // If there is a request from the D$ and we are not busy sending the
-    // response for the I$ we perform the D$ request
-    if (dcache_req_valid_ff & !wait_rsp_icache_ff)
-    begin
-        req_mm_valid = !rsp_mm_valid;
-        req_mm_info  = dcache_req_info_ff;
-
-        if(rsp_mm_valid)
-        begin
             // De-assert request to the MM
-            req_mm_valid    = 1'b0;
+        req_mm_valid    = 1'b0;
 
             // Response to the core
-            rsp_valid_miss  = 1'b1;
-            rsp_cache_id    = 1'b1; // response to D$
-            rsp_data_miss   = rsp_mm_data; 
-
-            // Reset control signal
-            dcache_req_valid_next   = dcache_req_valid_miss;
-        end
+        rsp_valid_miss  = 1'b1;
+        rsp_thread_id   = req_mm_info_ff.thread_id;
+        rsp_cache_id    = req_mm_info_is_dcache_ff; // 1 for D$ ; 0 for I$
+        rsp_data_miss   = rsp_mm_data; 
     end
 
-    // If there is a request from the I$ and not from the D$ or we are 
-    // already performing the I$ request we (continue) perform the I$ request
-    if ((!dcache_req_valid_ff & icache_req_valid_ff) | wait_rsp_icache_ff)
-    begin
-        wait_rsp_icache_next = 1'b1;
-        req_mm_valid = !rsp_mm_valid;
-        req_mm_info  = icache_req_info_ff;    
-
-        if(rsp_mm_valid)
-        begin
-            // De-assert request to the MM
-            req_mm_valid    = 1'b0;
-
-            // Response to the core                
-            rsp_valid_miss  = 1'b1;
-            rsp_cache_id    = 1'b0; // response to I$
-            rsp_data_miss   = rsp_mm_data;
-            
-            // Reset control signal
-            wait_rsp_icache_next    = 1'b0;
-            wait_icache_rsp_update  = 1'b1; 
-            icache_req_valid_next   = icache_req_valid_miss;
-        end
-    end
 end
 
 //////////////////////////////////////////////////
@@ -198,6 +201,7 @@ logic [`LATENCY_MM_RSP_RANGE] mem_rsp_count;
 always_ff @(posedge clk_i) 
 begin
     rsp_mm_valid     <= 1'b0;
+    rsp_mm_ready     <= 1'b1;
     rsp_mm_bus_error <= 1'b0;
 
     if (reset_i)
@@ -225,14 +229,16 @@ begin
         $display("------------------------------------------");          
         $display("------------------------------------------");          
     end
-    else if (req_mm_valid)
+    else if (req_mm_valid_ff)
     begin
+        rsp_mm_ready  <= 1'b0;
         mem_rsp_count <= mem_rsp_count + 1'b1;
 
         if (mem_rsp_count == `LATENCY_MM_RSP-1)
         begin
             // Send response to the core arbiter
             rsp_mm_valid  <= 1'b1;
+            rsp_mm_ready  <= 1'b1;
 
             if (req_mm_info_ff.addr >=  (`MAIN_MEMORY_DEPTH*`MAIN_MEMORY_LINE_SIZE) ) 
             begin
